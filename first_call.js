17c40e925bf072429f2b4b844d03a6cff3b1ff8c
gaurav-singh1 <grvsingh64@gmail.com>
2023-02-13 07:43:38 +0530

    list engines openai

---
+++ first_call.js
@@ -0,0 +1,11 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const OPENAI_API_KEY = "take from env";
+
+const configuration = new Configuration({
+    organization: "org-X3ujDHi2hBvWjQhquBGtEFtu",
+    apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines();
+console.log(response.data.data)
