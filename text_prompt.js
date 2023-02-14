e458bd9f87056da05d506e3e40aa8673eb4261bd
gaurav-singh1 <grvsingh64@gmail.com>
2023-02-14 07:05:22 +0530

    text prompt to function conversion

--- text_prompt.js
+++ text_prompt.js
@@ -1,13 +1,20 @@
-const { Configuration, OpenAIApi } = require("openai");
+/* eslint-disable camelcase */
+/* eslint-disable require-jsdoc */
+const {Configuration, OpenAIApi} = require('openai');
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "What are the names of the satellites of saturn?",
-  max_tokens: 50,
-  temperature: 0,
-});
 
-response.then(result => console.log(result.data.choices));
+function getSuggestions(text_prompt) {
+  const response = openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: text_prompt,
+    max_tokens: 50,
+    temperature: 0,
+  });
+  return response;
+}
+
+getSuggestions('was brexit really useful for britain?').then((result) =>
+  console.log(result.data.choices));
