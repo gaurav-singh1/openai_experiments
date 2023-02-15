c1cef5cfbf867e1df70c8274ef26debe52319268
gaurav-singh1 <grvsingh64@gmail.com>
2023-02-15 06:27:41 +0530

    local auth strategy workign

--- whatsapp_web_login.js
+++ whatsapp_web_login.js
@@ -1,51 +1,45 @@
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
-const { Client, LegacySessionAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+
 
 // Path where the session data will be stored
-const SESSION_FILE_PATH = './session.json';
+// const SESSION_FILE_PATH = './session.json';
 
 // Load the session data if it has been previously saved
-let sessionData;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-  sessionData = require(SESSION_FILE_PATH);
-}
+// let sessionData;
+// if (fs.existsSync(SESSION_FILE_PATH)) {
+//     sessionData = require(SESSION_FILE_PATH);
+// }
 
-// Use the saved values
 const client = new Client({
-  authStrategy: new LegacySessionAuth({
-    session: sessionData
-  })
+    authStrategy: new LocalAuth()
 });
+ 
 
 client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-  });
-
-// Save session values to the file upon successful auth
-client.on('authenticated', (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
+    qrcode.generate(qr, { small: true });
 });
+
 
 
 client.on('ready', () => {
-  console.log('Client is ready!');
-  client.getChats().then((chats) => {
-    console.log(chats[0]);
-  });
+    console.log('Client is ready!');
+    client.getChats().then((chats) => {
+        selected_chat = chats.filter((chat) => chat.name === 'Notes')
+        console.log(selected_chat);
+    });
 });
 
 client.initialize();
 
 
-client.on('message', (message) => {
-  console.log("message received  = ", message.body);
-  if (message.body === '!!ping') {
-    message.reply('pong');
-  }
+client.on('message', message => {
+    console.log("message arrived", message);
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
 });
+ 
+
