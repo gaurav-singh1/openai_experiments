31e4d6283ed73bc76c9248365483e89c14921661
gaurav-singh1 <grvsingh64@gmail.com>
2023-02-21 07:09:40 +0530

    whatsapp synced with openai

--- whatsapp_web_login.js
+++ whatsapp_web_login.js
@@ -6,7 +6,7 @@ const { getSuggestions } = require('./text_prompt');
 const client = new Client({
     authStrategy: new LocalAuth()
 });
- 
+
 
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
@@ -22,7 +22,7 @@ client.on('ready', () => {
         // if(notes_group[0].name==='Notes'){
         //     client.sendMessage(notes_group[0].id._serialized, "Test Message")
         // }
-        
+
     });
 });
 
@@ -30,15 +30,40 @@ client.initialize();
 client.on('message_create', message => {
     console.log("message created!!")
     console.log(message)
-    if(message.body.includes('ms/')) {
+    if (message.body.includes('ms/')) {
         console.log("attempting to search!")
         const searchQur = message.body.split("ms/")[1];
-        if(searchQur){
+        if (searchQur) {
             console.log("your search query is = ", searchQur)
-            getSuggestions(searchQur).then((result) =>
-            console.log(result.data.choices));
+            // const sampleRespose = [
+            //     {
+            //       text: '\n\nThe Prime Minister of India is Narendra Modi.',
+            //       index: 0,
+            //       logprobs: null,
+            //       finish_reason: 'stop'
+            //     }
+            //   ]
+            // client.getChats().then((chats) => {
+            //     const notes_group = chats.filter((chat) => chat.name === 'Notes')
+            //     console.log("*******", notes_group);
+            //     if (notes_group[0].name === 'Notes') {
+            //         client.sendMessage(notes_group[0].id._serialized, sampleRespose[0].text)
+            //     }
+            // });
+
+            getSuggestions(searchQur).then((result) => {
+                console.log(result.data.choices)
+                client.getChats().then((chats) => {
+                    const notes_group = chats.filter((chat) => chat.name === 'Notes')
+                    console.log("========>>>>>>>>", notes_group);
+                    if (notes_group[0].name === 'Notes') {
+                        client.sendMessage(notes_group[0].id._serialized, result.data.choices[0].text)
+                    }
+                });
+            }
+            );
         }
     }
 });
- 
+
 
