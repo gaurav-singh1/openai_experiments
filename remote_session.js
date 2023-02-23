279fb8b563bf0d42a885b6b5dc45f02b2826aaf2
gaurav-singh1 <grvsingh64@gmail.com>
2023-02-23 07:51:59 +0530

    remote auth successfull

--- remote_session.js
+++ remote_session.js
@@ -1,4 +1,13 @@
 const { Client, RemoteAuth } = require('whatsapp-web.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+const { getSuggestions } = require('./text_prompt');
+
+
+
 
 // Require database
 const { MongoStore } = require('wwebjs-mongo');
@@ -7,13 +16,29 @@ const mongoose = require('mongoose');
 // Load the session data
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     const store = new MongoStore({ mongoose: mongoose });
+    console.log("store initialized")
     const client = new Client({
         authStrategy: new RemoteAuth({
             store: store,
             backupSyncIntervalMs: 300000
         })
     });
-
+    client.on('qr', (qr) => {
+        qrcode.generate(qr, { small: true });
+    });    
+    console.log("client declared")
     client.initialize();
+    console.log("client initialized")
+    client.on('ready', () => {
+        console.log('Client is ready!');
+        client.getChats().then((chats) => {
+            const notes_group = chats.filter((chat) => chat.name === 'Notes')
+            console.log(notes_group);
+            // if(notes_group[0].name==='Notes'){
+            //     client.sendMessage(notes_group[0].id._serialized, "Test Message")
+            // }
+    
+        });
+    });
 });
  
