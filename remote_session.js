1ca9c7bc11a6a281ce3fb31ff8aa6b51b3f23b3f
gaurav-singh1 <grvsingh64@gmail.com>
2023-03-05 11:34:11 +0530

    error: getting qr code multiple times

--- remote_session.js
+++ remote_session.js
@@ -34,11 +34,56 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
         client.getChats().then((chats) => {
             const notes_group = chats.filter((chat) => chat.name === 'Notes')
             console.log(notes_group);
-            // if(notes_group[0].name==='Notes'){
-            //     client.sendMessage(notes_group[0].id._serialized, "Test Message")
-            // }
+            if(notes_group[0].name==='Notes'){
+                client.sendMessage(notes_group[0].id._serialized, "ms/good scifi novels over a weekend suggestions?")
+                
+            }
     
         });
+
+        client.on('message_create', message => {
+            console.log("message created!!")
+            console.log(message)
+            if (message.body.includes('ms/')) {
+                console.log("attempting to search!")
+                const searchQur = message.body.split("ms/")[1];
+                if (searchQur) {
+                    console.log("your search query is = ", searchQur)
+                    // const sampleRespose = [
+                    //     {
+                    //       text: '\n\nThe Prime Minister of India is Narendra Modi.',
+                    //       index: 0,
+                    //       logprobs: null,
+                    //       finish_reason: 'stop'
+                    //     }
+                    //   ]
+                    // client.getChats().then((chats) => {
+                    //     const notes_group = chats.filter((chat) => chat.name === 'Notes')
+                    //     console.log("*******", notes_group);
+                    //     if (notes_group[0].name === 'Notes') {
+                    //         client.sendMessage(notes_group[0].id._serialized, sampleRespose[0].text)
+                    //     }
+                    // });
+        
+                    getSuggestions(searchQur).then((result) => {
+                        console.log(result.data.choices)
+                        client.getChats().then((chats) => {
+                            const notes_group = chats.filter((chat) => chat.name === 'Notes')
+                            console.log("========>>>>>>>>", notes_group);
+                            if (notes_group[0].name === 'Notes') {
+                                client.sendMessage(notes_group[0].id._serialized, result.data.choices[0].text)
+                            }
+                        });
+                    }
+                    );
+                }
+            }
+        });
+
+
+
     });
 });
+
+
  
