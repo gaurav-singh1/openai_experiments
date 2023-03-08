e6e61738734bd180c399bcf8deb404c10e30ca02
gaurav-singh1 <grvsingh64@gmail.com>
2023-03-08 08:19:42 +0530

    removed verbose & using notes id to send message

--- remote_session.js
+++ remote_session.js
@@ -1,3 +1,4 @@
+console.log(">>>>>>>>>>>>>>>>>>>> running the main file now <<<<<<<<<<<<<<<<<<<<")
 const { Client, RemoteAuth } = require('whatsapp-web.js');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -6,8 +7,7 @@ const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 const { getSuggestions } = require('./text_prompt');
 
-
-
+const notes_group_id = "120363039263622605@g.us"
 
 // Require database
 const { MongoStore } = require('wwebjs-mongo');
@@ -31,17 +31,9 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("client initialized")
     client.on('ready', () => {
         console.log('Client is ready!');
-        client.getChats().then((chats) => {
-            const notes_group = chats.filter((chat) => chat.name === 'Notes')
-            console.log(notes_group);
-            // if (notes_group[0].name === 'Notes') {
-            //     client.sendMessage(notes_group[0].id._serialized, "ms/good scifi novels over a weekend suggestions?")
-            // }
-        });
     });
     client.on('message_create', message => {
-        console.log("message created!!")
-        console.log(message)
+        console.log("new message created!!")
         if (message.body.includes('ms/')) {
             console.log("attempting to search!")
             const searchQur = message.body.split("ms/")[1];
@@ -49,13 +41,7 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
                 console.log("your search query is = ", searchQur)
                 getSuggestions(searchQur).then((result) => {
                     console.log(result.data.choices)
-                    client.getChats().then((chats) => {
-                        const notes_group = chats.filter((chat) => chat.name === 'Notes')
-                        console.log("========>>>>>>>>", notes_group);
-                        if (notes_group[0].name === 'Notes') {
-                            client.sendMessage(notes_group[0].id._serialized, result.data.choices[0].text)
-                        }
-                    });
+                    client.sendMessage(notes_group_id, result.data.choices[0].text)
                 }
                 );
             }
